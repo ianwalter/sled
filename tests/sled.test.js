99305a41ac9e94a1e0695580496fa02a6d29b456
Ian Walter <public@iankwalter.com>
2018-10-18 20:16:27 -0400

    Adding travis config, switching to yarn

---
+++ tests/sled.test.js
@@ -0,0 +1,15 @@
+const Sled = require('..')
+
+describe('Sled', () => {
+  describe('.slide', () => {
+    it('should slide to a given height', () => {
+
+    })
+  })
+
+  describe('.toggle', () => {
+    it('should slide to a natural height', () => {
+
+    })
+  })
+})
